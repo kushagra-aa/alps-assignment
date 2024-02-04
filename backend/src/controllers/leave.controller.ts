d432e1c5e0708e63ea4704fdb235b1a70c4031fa
kushagra-aa <kushagraagnihotri24@gmial.com>
2024-02-04 12:30:32 +0530

    fixed api response in get leaves

--- backend/src/controllers/leave.controller.ts
+++ backend/src/controllers/leave.controller.ts
@@ -3,9 +3,10 @@ import ApiError from "../helpers/apiError.js";
 import ApiResponse from "../helpers/apiResponse.js";
 
 const getLeaveApplications = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "working!",
-  });
+  // if(error) {  throw new ApiError(400, "Something Went Wrong?")}
+  return res
+    .status(200)
+    .json(new ApiResponse(200, [], "Leave Applications found successfully"));
 });
 
 export { getLeaveApplications };
